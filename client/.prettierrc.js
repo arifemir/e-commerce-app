90183cb4337810a1ac941ab10b5d88ab4fd8dfd8
arifemir <arifarslanturk@gmail.com>
2021-01-03 05:59:53 +0300

    -client linters setup;

---
+++ client/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports =  {
+  semi:  false,
+  trailingComma:  'all',
+  singleQuote:  true,
+  printWidth:  120,
+  tabWidth:  2,
+  useTabs: false,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+};
