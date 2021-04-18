2e9ead40fc4a180c841a911e1177da266323a107
arifemir <arifarslanturk@gmail.com>
2021-04-18 20:21:26 +0300

    - unnecessary store user detail;

--- client/src/store/combineReducers.ts
+++ client/src/store/combineReducers.ts
@@ -5,7 +5,6 @@ import productList from './product-list/productListReducer';
 import productDetail from './product-detail/productDetailReducer';
 import cart from './cart/cartReducer';
 import userAuth from './user-auth/userAuthReducer';
-import userDetail from './user-detail/userDetailReducer';
 import shipping from './shipping/shippingReducer';
 import payment from './payment/paymentReducer';
 import order from './order/orderReducer';
@@ -15,7 +14,6 @@ export const rootReducer = combineReducers({
   productDetail,
   cart,
   userAuth,
-  userDetail,
   shipping,
   payment,
   order,
