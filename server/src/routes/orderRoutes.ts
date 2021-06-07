9e7f21f2dc84e9da6706c48a0e7eff78bd67f61e
DESKTOP-IF5K2A1\arifa <arifarslanturk@gmail.com>
2021-06-07 10:53:54 +0300

    - service implemented;

--- server/src/routes/orderRoutes.ts
+++ server/src/routes/orderRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { addOrderItems, getOrderItemById, getOrdersForUser, updateOrderItemToPaid } from '../controllers/orderController';
+import { addOrderItems, getAllOrder, getOrderItemById, getOrdersForUser, updateOrderItemToPaid, updateOrderToDelivered } from '../controllers/orderController';
+import { checkIsAdmin } from '../middleware/checkIsAdminMiddleware';
+import { takeTokenGiveUser } from '../middleware/tokenValidatorMiddleware';
 const router = express.Router();
 
 router.route('/').post(addOrderItems).get(getOrdersForUser);
 router.route('/:id').get(getOrderItemById);
 router.route('/:id/pay').put(updateOrderItemToPaid);
+router.put('/:id/deliver', takeTokenGiveUser, checkIsAdmin, updateOrderToDelivered);
+router.get('/allorder', takeTokenGiveUser, checkIsAdmin, getAllOrder);
 
 export default router;
