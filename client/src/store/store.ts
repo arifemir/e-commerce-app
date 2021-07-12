258bc4ae3096a8c2e1b7b791eab604016169be4c
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2021-07-12 14:44:46 +0300

    dependencies;

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from './combineReducers';
 import { saveAuthToken } from './reduxMiddlewares';
